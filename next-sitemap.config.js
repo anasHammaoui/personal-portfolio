d19c3455790fbd1cbaed537f89052f9015cca131
anashm <anashammaoui07@gmail.com>
2025-08-23 15:12:14 +0100

    update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://anashammaoui.me",
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
+  siteUrl: 'https://www.anashammaoui.me',
+  generateRobotsTxt: true, // optional
+  sitemapSize: 5000,
+  exclude: [
+    '/_app', 
+    '/_document',
+    '/_error',
+    '/api/*'
+  ],
+  transform: async (config, path) => {
+    // Optional: ensure no extra tags are injected
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
